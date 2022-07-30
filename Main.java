98a530d69c83bfd847b67d9815e3805063044a1f
ElenaSanMar <110239137+ElenaSanMar@users.noreply.github.com>
2022-07-30 13:36:13 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = suma(1, 2, 3);
+        System.out.print("El valor de la suma es: ");
+        System.out.println(resultado);
+
+        coche miCoche = new coche();
+        miCoche.IncrementarPuerta();
+        System.out.print("El numero de puertas del coche es: ");
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
+
+class coche{
+    public int puertas = 0;
+
+    public void IncrementarPuerta(){
+        this.puertas++;
+    }
+
+}
